f7308d1693ace601a9f656a2985a60c90613b174
ilabose <64930850+ilabose@users.noreply.github.com>
2020-09-17 22:52:21 +0200

    Delete VariablesFase1.js

--- VariablesFase1.js
+++
@@ -1,9 +0,0 @@
-            console.log("FASE1");
-            var nom = "Ilaria";
-            var cognom1 = "Boselli";
-            var cognom2 = "-"
-            var dia = 10;
-            var mes = 11;
-            var any = 1977;
-            console.log(nom + " " + cognom2 + " " + cognom1);
-            console.log(dia + '/' + mes + '/' + any);

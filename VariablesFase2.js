3e3ba8b66e4b6402ae85b9b709ceb75eaa8d299f
ilabose <64930850+ilabose@users.noreply.github.com>
2020-09-17 22:52:50 +0200

    Delete VariablesFase2.js

--- VariablesFase2.js
+++
@@ -1,4 +0,0 @@
-            const bisesto = 1948;
-            var traspas = 4;
-            var diferencia = ((1977-bisesto)/traspas);
-            document.getElementById("text").innerHTML= (diferencia);

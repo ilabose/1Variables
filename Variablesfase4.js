0f8c5653374fec9867248c2a6617ac2de1b643e1
ilabose <64930850+ilabose@users.noreply.github.com>
2020-09-17 22:55:28 +0200

    Delete Variablesfase4.js

--- Variablesfase4.js
+++
@@ -1,6 +0,0 @@
-            console.log("Anys de traspas de 1948 a 1977");
-            var bisesto = "Ilaria Boselli";
-            console.log("El meu nom és " + bisesto);
-            var cumple = "10/11/1977";
-            console.log("Vaig néixer el " + cumple);
-            console.log("El meu any de naixement no és de traspas ");
